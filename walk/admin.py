a021505897c82c33f38583ecf16d740f0e531266
한송희 <98_han@naver.com>
2022-07-24 20:48:51 +0900

    Refactor : custom admin class

--- walk/admin.py
+++ walk/admin.py
@@ -3,6 +3,18 @@ from django.contrib import admin
 from .models import Owner, Pet, Walk
 
 
-admin.site.register(Owner)
-admin.site.register(Pet)
-admin.site.register(Walk)
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class PetAdmin(admin.ModelAdmin):
+    list_display = ('id', 'breed', 'birth', 'name', 'owner_id')
+
+
+class WalkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'time', 'distance', 'pet_id')
+
+
+admin.site.register(Owner, OwnerAdmin)
+admin.site.register(Pet, PetAdmin)
+admin.site.register(Walk, WalkAdmin)
