0fb2edbdb3cac0d0c8235d52824e01c33a4e1a6e
한송희 <98_han@naver.com>
2022-07-28 19:33:19 +0900

    git reset

--- account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.urls import include
-
-
-urlpatterns = [
-    path('', include('dj_rest_auth.urls')),
-    path('', include('dj_rest_auth.registration.urls')),
-]
--- walk/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WalkConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'walk'
