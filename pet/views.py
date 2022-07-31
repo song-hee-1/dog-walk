546bf9fbc9dd2791e15369ff1036fd5eda489237
한송희 <98_han@naver.com>
2022-07-31 21:52:02 +0900

    Feat : implement walk list for each pet id

--- pet/views.py
+++ pet/views.py
@@ -1,9 +1,18 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from .models import Pet
-from .serializers import PetSerializer
+from .models import Pet, Walk
+from .serializers import PetSerializer, WalkSerializer
 
 
 class PetViewSet(viewsets.ModelViewSet):
     queryset = Pet.objects.all()
     serializer_class = PetSerializer
+
+    @action(detail=False, methods=['GET', 'POST'], url_path="^(?P<pk>[^/.]+)/walk")
+    def walk(self, request, **kwargs):
+        obj_id = self.kwargs['pk']
+        walk = Walk.objects.filter(pet_id=obj_id)
+        serializer = WalkSerializer(walk, many=True)
+        return Response(serializer.data)
