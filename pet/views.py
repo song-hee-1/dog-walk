a0a98791dac6ad1d9c88379f66278493cd30b542
한송희 <98_han@naver.com>
2022-08-02 20:29:45 +0900

    Feat : implementation of create walk log

--- pet/views.py
+++ pet/views.py
@@ -12,8 +12,17 @@ class PetViewSet(viewsets.ModelViewSet):
     serializer_class = PetSerializer
 
     @action(detail=False, methods=['GET', 'POST'], url_path="^(?P<pk>[^/.]+)/walk", serializer_class=WalkSerializer)
-    def walk(self, request, **kwargs):
-        obj_id = self.kwargs['pk']
-        walk = Walk.objects.filter(pet_id=obj_id)
-        serializer = WalkSerializer(walk, many=True)
-        return Response(serializer.data)
+    def get_walk(self, request, **kwargs):
+        if self.request.method == 'GET':
+            obj_id = self.kwargs['pk']
+            walk = Walk.objects.filter(pet_id=obj_id)
+            serializer = WalkSerializer(walk, many=True)
+            return Response(serializer.data)
+        elif self.request.method == 'POST':
+            serializer = WalkSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'status': 'new walklog set'})
+            else:
+                return Response(serializer.errors,
+                                status=status.HTTP_400_BAD_REQUEST)
