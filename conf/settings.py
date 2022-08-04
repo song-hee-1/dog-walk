a713474576def1cce43fd8776938a71548064d35
한송희 <98_han@naver.com>
2022-08-04 22:33:10 +0900

    Feat : change database sqlite to azure database for postgresql flexible server

--- conf/settings.py
+++ conf/settings.py
@@ -103,8 +103,12 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dogwalk',
+        'USER': os.environ['USER'],
+        'PASSWORD': os.environ['PASSWORD'],
+        'HOST': os.environ['HOST'],
+        'PORT': '5432',
     }
 }
 
