546a078bb7961e0b93c9fd84e2e88d62cbd7a2c8
한송희 <98_han@naver.com>
2022-08-04 23:21:20 +0900

    Fix : CSRF verification error - CSRF_trusted_origins to CSRF verification

--- conf/settings.py
+++ conf/settings.py
@@ -186,3 +186,5 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS= ['https://*.azurewebsites.net']
