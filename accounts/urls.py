4ba0b9a30c91429fc363e1eec66b5fb5a4b82816
한송희 <98_han@naver.com>
2022-08-07 21:30:37 +0900

    Fix : #2 email verification error - to handle account_confirm_email override confirmemailview

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.urls import include
 
+from .views import ConfirmEmailView
 
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('registration/', include('dj_rest_auth.registration.urls')),
+    re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(), name='account_confirm_email'),
 ]
